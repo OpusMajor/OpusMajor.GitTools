3b8353a2466bb7f4a6547e9c5a5f2e9fe9070dc7
Jens Horstmeier <jens@horstmeier.de>
2022-08-15 20:18:32 +0200

    Initial version

---
+++ gitrunner.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using OpusMajor.LibProcess;
+
+namespace OpusMajor.GitTools;
+
+public class GitRunner
+{
+    private readonly ExternalApplication _git;
+
+    public GitRunner(IProcessRunner processRunner, ILogger<GitRunner>? log = null)
+    {
+        _git = new ExternalApplication(processRunner, "git", null,
+            s => log?.LogDebug("GIT: {Message}", s),
+            s => log?.LogWarning("GITERR: {Message}", s),
+            n => n == 0);
+    }
+
+    public async Task Execute(string directory, params string[] arguments)
+    {
+        await _git.Run(arguments, directory);
+    }
+
+}
